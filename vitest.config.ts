bd459870c39457644520b600ddede7b6ae5c2a79
Steffen Schön <steffen1995@gmail.com>
2026-07-27 19:16:05 +0200

    add codecov

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
       provider: "v8",
       include: ["src/**/*.ts"],
       exclude: ["src/**/*.test.ts"],
-      reporter: ["text", "html", "json-summary"],
+      reporter: ["text", "html", "json-summary", "lcov"],
       reportsDirectory: "coverage",
     },
   },
